eb97fb116b1cedbef841255fb3e1092e6eb74873
awidney <adamwidney@hotmail.ca>
2022-07-08 13:47:54 -0700

    Update selector, variable name

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const sidebar_links = document.querySelectorAll('.sidebar a');
+const nav_links = document.querySelectorAll('nav a');
 
-sidebar_links.forEach((link) => {
+nav_links.forEach((link) => {
     link.addEventListener('mouseover', () => {
         link.children[0].children[0].classList.add('color-fill');
         link.children[0].classList.add('scale');
     });
 });
 
-sidebar_links.forEach((link) => {
+nav_links.forEach((link) => {
     link.addEventListener('mouseleave', () => {
         link.children[0].children[0].classList.remove('color-fill');
         link.children[0].classList.remove('scale');
